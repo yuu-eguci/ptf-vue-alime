8031761d4a01d1505ad8f94f277fe3a0373bb549
yuu-eguci <yuu-eguci@users.noreply.github.com>
2020-10-24 04:18:41 +0000

    deploy: 9d06dbbfb31c32ca749c46a224d25e8a943bd952

---
+++ js/chunk-2d22c862.c3b84496.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22c862"],{f431:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{attrs:{id:"locale-parent"}},[t("router-view")],1)},r=[],c={name:"LocaleParent"},l=c,u=t("2877"),o=Object(u["a"])(l,a,r,!1,null,null,null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d22c862.c3b84496.js.map
