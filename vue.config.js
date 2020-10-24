ed4000d7156a51d5f4599ead8d3445a8b1a0b7ee
yuu-eguci <yuu.eguci@gmail.com>
2020-10-24 12:11:30 +0900

    #5 ➕ i18n
    vue add i18n により全体的に src が自動編集されます。

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,12 @@ module.exports = {
   // ./ にすると assets は解決する。だけど spa が解決しなかった。
   // NOTE: カスタムドメインでの運用が始まったら消すこと。 -> 始まりました。
   // publicPath: '/ptf-vue-alime/'
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
 }
